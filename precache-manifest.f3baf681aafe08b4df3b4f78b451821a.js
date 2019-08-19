8875e8cd9bc100562f45c37c2db369aba26d7c85
maggievu <maggievu91@gmail.com>
2019-08-18 19:11:00 -0700

    Updates

---
+++ precache-manifest.f3baf681aafe08b4df3b4f78b451821a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed313161fdb1d67c00091294057b05d2",
+    "url": "/founders-reserve/index.html"
+  },
+  {
+    "revision": "c9286624c2398270280e",
+    "url": "/founders-reserve/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b33578193e34896dd5f2",
+    "url": "/founders-reserve/static/css/main.02cadcbb.chunk.css"
+  },
+  {
+    "revision": "c9286624c2398270280e",
+    "url": "/founders-reserve/static/js/2.013c42f2.chunk.js"
+  },
+  {
+    "revision": "b33578193e34896dd5f2",
+    "url": "/founders-reserve/static/js/main.b62b8828.chunk.js"
+  },
+  {
+    "revision": "8d98abc1c74eec41a101",
+    "url": "/founders-reserve/static/js/runtime~main.a8e5d1fb.js"
+  },
+  {
+    "revision": "b63023f13c185b980e24a0976540b24a",
+    "url": "/founders-reserve/static/media/adventure-alpine-background-355747.b63023f1.png"
+  },
+  {
+    "revision": "12b46222b0409ddb350a5e6c2537c17a",
+    "url": "/founders-reserve/static/media/first_screen1.12b46222.png"
+  },
+  {
+    "revision": "f3fd39582360e1b255b4b63810fad5ef",
+    "url": "/founders-reserve/static/media/first_screen2.f3fd3958.png"
+  },
+  {
+    "revision": "8079c1c67fc384bfcf6a88308f686383",
+    "url": "/founders-reserve/static/media/first_screen3.8079c1c6.png"
+  },
+  {
+    "revision": "f4d99892e564fef0732b116b0f8cfe78",
+    "url": "/founders-reserve/static/media/image10.f4d99892.png"
+  }
+]);
